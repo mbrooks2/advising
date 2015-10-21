baaa2cb52ce5c3e2f37c17a1de8dd436a1b95173
Mitch Almon Brooks <mbrooks2@silk3.uvm.edu>
2015-10-21 13:09:52 -0400

    copy assign1

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Mitch Brooks.</p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+
+<header>
+    <figure class="float_left small">
+            <img alt="calvin and hobbes has influenced my snowman making." src="images/snowmen.jpg">
+            <figcaption>Snowmen observing an accident.</figcaption>
+    </figure>
+    <h1>Mitch's Sample Tables</h1>
+    <h2>UVM Courses</h2>
+</header>
+
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
